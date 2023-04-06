d3d691afb69ad77d7e0ed7757594308f413c2263
arun raj <113658607+arun-261@users.noreply.github.com>
2023-04-06 15:42:54 +0530

    'bankv1'

---
+++ bank/src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+// loginform
+loginForm=this.fb.group({
+  acno:['',[Validators.required,Validators.pattern('[0-9]*')]],
+  pswd:['',[Validators.required,Validators.pattern('[a-zA-Z0-9]*')]]
+})
+
+ constructor(private fb:FormBuilder){}
+
+ ngOnInit(): void{
+  
+ }
+
+ login(){
+  
+  if(this.loginForm.valid){
+  let acno=this.loginForm.value.acno
+  let pswd=this.loginForm.value.pswd
+ 
+  console.log(acno);
+  console.log(pswd);
+  alert('login clicked')
+  }
+  else{
+    alert('invalid')
+  }
+  
+  
+ }
+}
